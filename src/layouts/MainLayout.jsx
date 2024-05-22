2cec1436a053bd390a6d83b1602ed52fda083b66
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-05-22 23:03:47 +0900

    header removed

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
 
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer/Footer';
-import HeaderBanner from '../components/Header/HeaderBanner';
+
 import TopNavbar from '../components/TopNavbar/TopNavbar';
 import classes from './MainLayout.module.css';
 
@@ -9,7 +9,6 @@ import classes from './MainLayout.module.css';
 function MainLayout(){
   return (
     <>
-      <HeaderBanner/>
       <TopNavbar/>
       <main className={classes.content}>
         <Outlet/>
